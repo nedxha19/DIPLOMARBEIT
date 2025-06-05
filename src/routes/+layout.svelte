3bcc6cf43ee29262d4f24e5d79886e4dfb16e769
Nedit <nedxha19@htl-shkoder.com>
2025-06-05 20:27:09 +0200

    Layout svelte file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
+</script>
+
+{#if !$page.url.pathname.startsWith('/admin')}
+	<Navbar />
+{/if}
+
+<main class="main">
+	<slot />
+</main>
+
+{#if !$page.url.pathname.startsWith('/admin')}
+	<Footer />
+{/if}
+
+<style>
+	main.main {
+		margin-left: 20px;
+		margin-right: 20px;
+	}
+
+	@media (max-width: 480px) {
+		main.main {
+			margin: 80px;
+		}
+	}
+</style>
