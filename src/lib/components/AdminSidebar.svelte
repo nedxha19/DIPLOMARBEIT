f227a66ac8e842da628c9756d77de4d0ccb85e0b
Nedit <nedxha19@htl-shkoder.com>
2025-06-06 12:23:56 +0200

    Imported necessary Svelte functions and Font Awesome styles. Set up variables to keep track of which menu item is active

---
+++ src/lib/components/AdminSidebar.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
+  import '@fortawesome/fontawesome-free/css/all.min.css';
+
+  export let isSidebarHidden;
+
+  let activeIndex = 0;
+  let hoverTimeout;
+  let hoveredItem = null;
+</script>
